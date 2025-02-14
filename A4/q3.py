89014da4d3e55eaac9750b5a2b4a456dc8417c5f
Your Name <bob85670@connect.hku.hk>
2025-02-15 00:07:56 +0800

    amend parsing input

--- A4/q3.py
+++ A4/q3.py
@@ -1,17 +1,19 @@
-
-
 def main():
     treasure = []
     first_input_treasure = input()
-    treasure.append(list(map(int, first_input_treasure)))
-    i = len(first_input_treasure) - 1
-    while (i):
-        if (i == 0):
-            break
-        treasure.append(list(map(int, input())))
-        i -= 1
+    treasure.append([int(x) for x in first_input_treasure])
+    for i in range(len(first_input_treasure) - 1):
+        treasure.append([int(x) for x in input()])
     print(treasure)
+    
+    game_map = []
+    first_input_map = input()
+    game_map.append([int(x) for x in first_input_map])
+    for i in range(len(first_input_map) - 1):
+        game_map.append([int(x) for x in input()])
+    print(game_map)
 
+    
 
 if __name__ == "__main__":
     main()
